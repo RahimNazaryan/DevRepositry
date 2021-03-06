5a735794f4cbfa705caf14aadfe8a96db88b90a9
Rahim <nazaryanrahim@gmail.com>
2021-03-06 12:13:36 -0500

    2nd commit

--- src/main/java/com/class1/Main.java
+++ src/main/java/com/class1/Main.java
@@ -3,7 +3,7 @@ package com.class1;
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i < 5 ; i++) {
+        for (int i = 0; i < 4 ; i++) {
             System.out.println("Hello world");
             try {
                 Thread.sleep(2000);
